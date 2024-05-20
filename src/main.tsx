a90412b6d0495e2e57a1506bb27635bb572e5388
mancuoj <mancuoj@gmail.com>
2024-05-20 21:05:04 +0800

    feat: router config

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,31 @@
 import '@fontsource-variable/inter'
 import '@fontsource-variable/source-serif-4'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { RouterProvider, createRouter } from '@tanstack/react-router'
+import { ErrorComponent, RouterProvider, createRouter } from '@tanstack/react-router'
 import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
 import { routeTree } from './routeTree.gen'
 import '~/styles/index.css'
+
+export const queryClient = new QueryClient()
+
+const router = createRouter({
+  routeTree,
+  defaultPendingComponent: () => <div>Loading...</div>,
+  defaultErrorComponent: ({ error }) => <ErrorComponent error={error} />,
+  context: {
+    auth: undefined,
+    queryClient,
+  },
+  defaultPreload: 'intent',
+  defaultPreloadStaleTime: 0,
+})
 
 declare module '@tanstack/react-router' {
   interface Register {
     router: typeof router
   }
 }
-
-const queryClient = new QueryClient()
-const router = createRouter({ routeTree })
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <StrictMode>
