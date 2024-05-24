1efe1944c401eefe686d2fedfe43999cdf857721
mancuoj <mancuoj@gmail.com>
2024-05-25 00:01:00 +0800

    feat: settings dialog

--- src/main.tsx
+++ src/main.tsx
@@ -10,6 +10,7 @@ import { useSessionStorage } from '~/hooks/use-session-storage'
 import { routeTree } from '~/routeTree.gen'
 import { auth } from '~/utils/auth'
 import '~/styles/index.css'
+import { Dialog, DialogContent, DialogTrigger } from './components/ui/dialog'
 import { Separator } from './components/ui/separator'
 
 export const queryClient = new QueryClient()
@@ -43,39 +44,53 @@ function App() {
 
   return (
     <>
-      <Card className="fixed bottom-2 left-2 w-56 text-sm">
-        <div className="space-y-3 p-3">
-          <div className="flex gap-1">
-            <Button size="sm" onClick={() => setLoaderDelay(150)}>
-              Fast
-            </Button>
-            <Button size="sm" onClick={() => setLoaderDelay(500)}>
-              Fast 3G
-            </Button>
-            <Button size="sm" onClick={() => setLoaderDelay(2000)}>
-              Slow 3G
-            </Button>
+      <Dialog>
+        <DialogTrigger asChild>
+          <Button variant="outline" className="fixed bottom-3 right-3">
+            <span className="i-lucide-settings mr-2" />
+            Settings
+          </Button>
+        </DialogTrigger>
+        <DialogContent>
+          <div className="flex flex-col gap-5 p-3">
+            <div className="flex gap-3">
+              <Button onClick={() => setLoaderDelay(150)}>Fast</Button>
+              <Button onClick={() => setLoaderDelay(500)}>Fast 3G</Button>
+              <Button onClick={() => setLoaderDelay(2000)}>Slow 3G</Button>
+            </div>
+            <div className="space-y-3">
+              <div>Loader Delay: {loaderDelay}ms</div>
+              <Slider
+                value={[loaderDelay]}
+                onValueChange={([value]) => setLoaderDelay(value)}
+                min={0}
+                max={5000}
+                step={100}
+              />
+            </div>
+            <div className="space-y-3">
+              <div>PendingMs: {pendingMs}ms</div>
+              <Slider
+                value={[pendingMs]}
+                onValueChange={([value]) => setPendingMs(value)}
+                min={0}
+                max={5000}
+                step={100}
+              />
+            </div>
+            <div className="space-y-3">
+              <div>PendingMinMs: {pendingMinMs}ms</div>
+              <Slider
+                value={[pendingMinMs]}
+                onValueChange={([value]) => setPendingMinMs(value)}
+                min={0}
+                max={5000}
+                step={100}
+              />
+            </div>
           </div>
-          <div>Loader Delay: {loaderDelay}ms</div>
-          <Slider
-            value={[loaderDelay]}
-            onValueChange={([value]) => setLoaderDelay(value)}
-            min={0}
-            max={5000}
-            step={100}
-          />
-          <div>PendingMs: {pendingMs}ms</div>
-          <Slider value={[pendingMs]} onValueChange={([value]) => setPendingMs(value)} min={0} max={5000} step={100} />
-          <div>PendingMinMs: {pendingMinMs}ms</div>
-          <Slider
-            value={[pendingMinMs]}
-            onValueChange={([value]) => setPendingMinMs(value)}
-            min={0}
-            max={5000}
-            step={100}
-          />
-        </div>
-      </Card>
+        </DialogContent>
+      </Dialog>
       <RouterProvider
         router={router}
         defaultPreload="intent"
