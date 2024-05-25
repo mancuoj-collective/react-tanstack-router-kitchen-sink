2cd946d0278d41a3bf00f23134d84279c5a05e55
mancuoj <mancuoj@gmail.com>
2024-05-26 02:46:05 +0800

    feat: update invoice

--- src/routes/dashboard/invoices/$invoiceId.tsx
+++ src/routes/dashboard/invoices/$invoiceId.tsx
@@ -1,9 +1,11 @@
 import { useSuspenseQuery } from '@tanstack/react-query'
 import { createFileRoute } from '@tanstack/react-router'
+import { useEffect } from 'react'
+import { toast } from 'sonner'
 import { z } from 'zod'
 import { InvoiceFields } from '~/components/invoice-fields'
 import { Button } from '~/components/ui/button'
-import { invoiceQueryOptions } from '~/utils/api'
+import { invoiceQueryOptions, useUpdateInvoiceMutation } from '~/utils/api'
 
 export const Route = createFileRoute('/dashboard/invoices/$invoiceId')({
   parseParams: (params) => ({
@@ -18,17 +20,38 @@ function InvoiceComponent() {
   const params = Route.useParams()
   const invoiceQuery = useSuspenseQuery(invoiceQueryOptions(params.invoiceId))
   const invoice = invoiceQuery.data
+  const updateInvoiceMutation = useUpdateInvoiceMutation(params.invoiceId)
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
     e.stopPropagation()
+    const formData = new FormData(e.target as HTMLFormElement)
+    updateInvoiceMutation.mutate({
+      id: invoice.id,
+      title: formData.get('title') as string,
+      body: formData.get('body') as string,
+    })
   }
 
+  useEffect(() => {
+    if (updateInvoiceMutation.status === 'success') {
+      toast.success('Updated successfully')
+    } else if (updateInvoiceMutation.status === 'error') {
+      toast.error('Failed to update')
+    }
+  }, [updateInvoiceMutation.status])
+
   return (
-    <form key={invoice.id} className="space-y-3 p-3" onSubmit={(e) => handleSubmit(e)}>
-      <InvoiceFields invoice={invoice} />
+    <form key={invoice.id} className="space-y-3 p-3" onSubmit={handleSubmit}>
+      <InvoiceFields invoice={invoice} disabled={updateInvoiceMutation.status === 'pending'} />
       <Button type="submit" variant="outline" className="w-full">
-        Save
+        {updateInvoiceMutation.status === 'pending' ? (
+          <>
+            <span className="i-lucide-loader-circle mr-3 animate-spin text-lg" /> Saving
+          </>
+        ) : (
+          <>Save</>
+        )}
       </Button>
     </form>
   )
